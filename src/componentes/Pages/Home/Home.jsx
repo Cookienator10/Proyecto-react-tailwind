623d0214849109f904318a9071658fc8dfde72c6
Cookienator10 <santiagorojas772@gmail.com>
2024-05-09 09:43:22 -0500

    Se crearon los archgivos y rutas principales del proyecto react con tailwind

---
+++ src/componentes/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header} from '../../Layouts/Header/Header';
+import { Main} from '../../Layouts/Main/Main';
+import { Header} from '../../Layouts/Footer/Footer';
+
+export const Home = () => {
+  return (
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
+
+  )
+}
